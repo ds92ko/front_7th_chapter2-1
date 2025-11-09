d864fa00ad9028ee0dbed67033f1b0fae9c71f11
dasom <ds92ko@gmail.com>
2025-11-10 06:49:08 +0900

    feat: layout 컴포넌트 개발
    - header 컴포넌트 개발
    - footer 컴포넌트 개발

---
+++ src/pages/Layout.js
@@ -0,0 +1,19 @@
+import { Footer, Header } from '@/components';
+
+/**
+ * 레이아웃 컴포넌트
+ *
+ * @param {{ children: string }} props
+ * @returns {string}
+ */
+export const Layout = ({ children }) => {
+  return /* html */ `
+    <div class="min-h-screen bg-gray-50">
+      ${Header()}
+      <main class="max-w-md mx-auto px-4 py-4">
+        ${children}
+      </main>
+      ${Footer()}
+    </div>
+  `;
+};
