6d3be62ef20d4fdeed59ddb93325ae7517d998c2
dasomko <ds92ko@gmail.com>
2025-11-10 12:00:41 +0900

    chore: gh-pages 설치 및 배포용 Vite base 경로 추가

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from 'url';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/front_7th_chapter2-1/',
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.js",
-    exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    exclude: ['**/e2e/**', '**/*.e2e.spec.js', '**/node_modules/**'],
     poolOptions: {
       threads: {
         singleThread: true,
